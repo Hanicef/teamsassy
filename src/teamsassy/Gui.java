c550f1d6d007a2b4108e035924008e66597ae152
Sebastian Kihlstrand <sebastian.kihlstrand@hotmail.com>
2017-11-28 11:48:51 +0100

    Add images for all cards

--- src/teamsassy/Gui.java
+++ src/teamsassy/Gui.java
@@ -1,5 +1,6 @@
 package teamsassy;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
@@ -40,7 +41,7 @@ public class Gui extends JFrame implements ActionListener {
 	private JCheckBox c5 = new JCheckBox();
 
 	private JPanel textMessagePanel = new JPanel();
-	private JTextArea textArea = new JTextArea(3, 35);
+	private static JTextArea textArea = new JTextArea(3, 35);
 
 	private JPanel cardPanel = new JPanel();
 
@@ -50,10 +51,68 @@ public class Gui extends JFrame implements ActionListener {
 	private JLabel card4 = new JLabel();
 	private JLabel card5 = new JLabel();
 
-	private ImageIcon spadesAce = new ImageIcon("src/images/Ace_of_spades.png");
-
+	private ImageIcon back = new ImageIcon("src/images/back.png");
+	
+	private ImageIcon spadesAce = new ImageIcon("src/images/ace_of_spades.png");
+	private ImageIcon spades2 = new ImageIcon("src/images/2_of_spades.png");
+	private ImageIcon spades3 = new ImageIcon("src/images/3_of_spades.png");
+	private ImageIcon spades4 = new ImageIcon("src/images/4_of_spades.png");
+	private ImageIcon spades5 = new ImageIcon("src/images/5_of_spades.png");
+	private ImageIcon spades6 = new ImageIcon("src/images/6_of_spades.png");
+	private ImageIcon spades7 = new ImageIcon("src/images/7_of_spades.png");
+	private ImageIcon spades8 = new ImageIcon("src/images/8_of_spades.png");
+	private ImageIcon spades9 = new ImageIcon("src/images/9_of_spades.png");
+	private ImageIcon spades10 = new ImageIcon("src/images/10_of_spades.png");
+	private ImageIcon spadesKnight = new ImageIcon("src/images/knight_of_spades.png");
+	private ImageIcon spadesQueen = new ImageIcon("src/images/queen_of_spades.png");
+	private ImageIcon spadesKing = new ImageIcon("src/images/king_of_spades.png");
+	
+	private ImageIcon clubsAce = new ImageIcon("src/images/ace_of_clubs.png");
+	private ImageIcon clubs2 = new ImageIcon("src/images/2_of_clubs.png");
+	private ImageIcon clubs3 = new ImageIcon("src/images/3_of_clubs.png");
+	private ImageIcon clubs4 = new ImageIcon("src/images/4_of_clubs.png");
+	private ImageIcon clubs5 = new ImageIcon("src/images/5_of_clubs.png");
+	private ImageIcon clubs6 = new ImageIcon("src/images/6_of_clubs.png");
+	private ImageIcon clubs7 = new ImageIcon("src/images/7_of_clubs.png");
+	private ImageIcon clubs8 = new ImageIcon("src/images/8_of_clubs.png");
+	private ImageIcon clubs9 = new ImageIcon("src/images/9_of_clubs.png");
+	private ImageIcon clubs10 = new ImageIcon("src/images/10_of_clubs.png");
+	private ImageIcon clubsKnight = new ImageIcon("src/images/knight_of_clubs.png");
+	private ImageIcon clubsQueen = new ImageIcon("src/images/queen_of_clubs.png");
+	private ImageIcon clubsKing = new ImageIcon("src/images/king_of_clubs.png");
+	
+	private ImageIcon diamondsAce = new ImageIcon("src/images/ace_of_diamonds.png");
+	private ImageIcon diamonds2 = new ImageIcon("src/images/2_of_diamonds.png");
+	private ImageIcon diamonds3 = new ImageIcon("src/images/3_of_diamonds.png");
+	private ImageIcon diamonds4 = new ImageIcon("src/images/4_of_diamonds.png");
+	private ImageIcon diamonds5 = new ImageIcon("src/images/5_of_diamonds.png");
+	private ImageIcon diamonds6 = new ImageIcon("src/images/6_of_diamonds.png");
+	private ImageIcon diamonds7 = new ImageIcon("src/images/7_of_diamonds.png");
+	private ImageIcon diamonds8 = new ImageIcon("src/images/8_of_diamonds.png");
+	private ImageIcon diamonds9 = new ImageIcon("src/images/9_of_diamonds.png");
+	private ImageIcon diamonds10 = new ImageIcon("src/images/10_of_diamonds.png");
+	private ImageIcon diamondsKnight = new ImageIcon("src/images/knight_of_diamonds.png");
+	private ImageIcon diamondsQueen = new ImageIcon("src/images/queen_of_diamonds.png");
+	private ImageIcon diamondsKing = new ImageIcon("src/images/king_of_diamonds.png");
+	
+	private ImageIcon heartsAce = new ImageIcon("src/images/ace_of_hearts.png");
+	private ImageIcon hearts2 = new ImageIcon("src/images/2_of_hearts.png");
+	private ImageIcon hearts3 = new ImageIcon("src/images/3_of_hearts.png");
+	private ImageIcon hearts4 = new ImageIcon("src/images/4_of_hearts.png");
+	private ImageIcon hearts5 = new ImageIcon("src/images/5_of_hearts.png");
+	private ImageIcon hearts6 = new ImageIcon("src/images/6_of_hearts.png");
+	private ImageIcon hearts7 = new ImageIcon("src/images/7_of_hearts.png");
+	private ImageIcon hearts8 = new ImageIcon("src/images/8_of_hearts.png");
+	private ImageIcon hearts9 = new ImageIcon("src/images/9_of_hearts.png");
+	private ImageIcon hearts10 = new ImageIcon("src/images/10_of_hearts.png");
+	private ImageIcon heartsKnight = new ImageIcon("src/images/knight_of_hearts.png");
+	private ImageIcon heartsQueen = new ImageIcon("src/images/queen_of_hearts.png");
+	private ImageIcon heartsKing = new ImageIcon("src/images/ace_of_king.png");
+	
 	public Gui() {
 		super("VideoPoker");
+		
+		//Ställa in och adda knapparna
 		Font buttonFont = new Font("Helvetica", Font.BOLD, 24);
 		Dimension buttonDim = new Dimension(120, 55);
 		start.setFont(buttonFont);
@@ -65,7 +124,6 @@ public class Gui extends JFrame implements ActionListener {
 		hold.setFont(buttonFont);
 		hold.setPreferredSize(buttonDim);
 		hold.addActionListener(this);
-
 		buttonPanel.setLayout(new GridBagLayout());
 		GridBagConstraints buttonPanelGBC = new GridBagConstraints();
 		buttonPanelGBC.insets = new Insets(0, 15, 0, 15);
@@ -76,7 +134,6 @@ public class Gui extends JFrame implements ActionListener {
 		buttonPanel.add(swap, buttonPanelGBC);
 		buttonPanelGBC.gridx = 3;
 		buttonPanel.add(hold, buttonPanelGBC);
-		
 		buttonPanelGBC.gridheight = 1;
 		buttonPanelGBC.gridx = 4;
 		buttonPanel.add(radio1, buttonPanelGBC);
@@ -87,6 +144,7 @@ public class Gui extends JFrame implements ActionListener {
 		radio1.addActionListener(this);
 		radio2.addActionListener(this);
 
+		//Ställa in och adda checkboxes
 		checkboxPanel.setLayout(new GridBagLayout());
 		GridBagConstraints checkboxPanelGBC = new GridBagConstraints();
 		checkboxPanelGBC.insets = new Insets(0, 40, 0, 40);
@@ -106,20 +164,22 @@ public class Gui extends JFrame implements ActionListener {
 		c4.addActionListener(this);
 		c5.addActionListener(this);
 
+		//Lägg till textArea
 		textArea.setOpaque(false);
 		textArea.setFont(new Font("Helvetica", Font.BOLD, 18));
 		textArea.setText("Du fick kåk, 1000 poäng till dig!");
 		textMessagePanel.add(textArea);
 
+		//Ställa in korten
 		card1.setIcon(spadesAce);
-		card2.setIcon(spadesAce);
-		card3.setIcon(spadesAce);
-		card4.setIcon(spadesAce);
-		card5.setIcon(spadesAce);
-
+		card2.setIcon(diamondsQueen);
+		card3.setIcon(clubs9);
+		card4.setIcon(spadesKnight);
+		card5.setIcon(hearts10	);
+		
+		//Lägga till korten i kortpanelen
 		cardPanel.setLayout(new GridBagLayout());
 		GridBagConstraints cardPanelGBC = new GridBagConstraints();
-
 		cardPanelGBC.insets = new Insets(5, 5, 5, 5);
 		cardPanelGBC.gridx = 1;
 		cardPanel.add(card1, cardPanelGBC);
@@ -135,36 +195,39 @@ public class Gui extends JFrame implements ActionListener {
 		GridBagConstraints frameGBC = new GridBagConstraints();
 
 		frameGBC.insets = new Insets(10, 0, 10, 0);
-
+		//Adda till text i framen
 		frameGBC.gridx = 1;
 		frameGBC.gridy = 1;
 		frameGBC.gridwidth = 5;
 		frameGBC.gridheight = 1;
 		add(textArea, frameGBC);
-
+		//Adda till cardPanel i framen
 		frameGBC.gridx = 1;
 		frameGBC.gridy = 2;
 		frameGBC.gridwidth = 5;
 		frameGBC.gridheight = 3;
 		add(cardPanel, frameGBC);
-
+		//Adda till checkboxPanel i framen
 		frameGBC.gridx = 1;
 		frameGBC.gridy = 6;
 		frameGBC.gridwidth = 5;
 		frameGBC.gridheight = 1;
 		add(checkboxPanel, frameGBC);
-
+		//Adda till buttonPanel i framen
 		frameGBC.gridx = 1;
 		frameGBC.gridy = 7;
 		frameGBC.gridwidth = 5;
 		frameGBC.gridheight = 1;
 		add(buttonPanel, frameGBC);
-
+		
+		//inställningar för Framen	
+		//getContentPane().setBackground(Color.DARK_GRAY);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		pack();
 		setVisible(true);
 	}
 
+	//Actionlistener
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
@@ -211,11 +274,23 @@ public class Gui extends JFrame implements ActionListener {
 			//Avmarkera kort 5för byte
 		}
 		if (radio1.isSelected()) {
-			textArea.setText("radio1 is selected");
+			//väljer att ett kort ska swapas			
 		}
 		if(radio2.isSelected()) {
-			textArea.setText("radio2 is selected");
+			//väljer att två kort ska swapas
 		}
+	}
+	
+	public static void setText(String text) {
+		textArea.setText(text);
+	}
+	
+	public static void setIconsForHand(Hand hand) {
+		
+	}
+	
+	public static void setIconsForSwap(Card card) {
+		
 	}
 
 }
