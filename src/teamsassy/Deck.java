ff3bcb344546c69c78c63cffd09ecef74b702dba
Yoella <joella.j@hotmail.com>
2017-11-28 14:30:45 +0100

    En start till Deck klassen

---
+++ src/teamsassy/Deck.java
@@ -0,0 +1,15 @@
+package teamsassy;
+
+public class Deck {
+	private Card[] cards;
+	
+	public Deck() {
+		cards = new Card[52];
+	}
+	public Card drawCard() {
+		return null;
+	}
+	public void reset() {
+		
+	}
+}
