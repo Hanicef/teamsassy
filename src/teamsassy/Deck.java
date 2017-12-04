b7245b9418fa9f65c7b44084d0d71a287230930f
Hanicef <gustaf.alhall@yahoo.com>
2017-12-04 11:27:51 +0100

    Rewrote save/load system

--- src/teamsassy/Deck.java
+++ src/teamsassy/Deck.java
@@ -1,6 +1,6 @@
 package teamsassy;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.ArrayList;
 //import java.util.Collections;
 import java.util.List;
@@ -44,4 +44,21 @@ public class Deck implements Serializable {
 		return s;
 		
 	}
+
+        public void writeData(DataOutputStream s) throws IOException {
+                s.writeInt(cards.size());
+                for (int i = 0; i < cards.size(); ++i) {
+                        s.writeInt(cards.get(i).getValue());
+                        s.writeInt(cards.get(i).getSuit().ordinal());
+                }
+        }
+
+        public void readData(DataInputStream s) throws IOException {
+                cards = new ArrayList<Card>();
+                int len = s.readInt();
+                for (int i = 0; i < len; ++i) {
+                        Card card = new Card(s.readInt(), Suit.values()[s.readInt()]);
+                        cards.add(card);
+                }
+        }
 }
